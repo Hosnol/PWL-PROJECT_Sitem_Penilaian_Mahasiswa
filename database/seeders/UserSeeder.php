00c42ce77202d3cc58bb196f1333e42a5c6a6a6e
Hosnol Arifin <hosnolarifin220@gmail.com>
2021-05-16 18:39:34 +0700

    Edit file user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -13,6 +14,28 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $admin = User::create([
+            'name' => 'Hosnol Arifin',
+            'email' => 'hosnolarifin87@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        $admin->assignRole('admin');
+
+        $dosen = User::create([
+            'name' => 'Zawarudddin',
+            'email' => 'dosen123@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        $dosen->assignRole('dosen');
+
+        $mhs = User::create([
+            'name' => 'Erika',
+            'email' => 'erika78@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        $mhs->assignRole('mahasiswa');
     }
 }
