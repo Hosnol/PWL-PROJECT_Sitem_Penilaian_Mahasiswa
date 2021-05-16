8cc3fef9c70feb1080362bd10160119ce6f357f5
Hosnol Arifin <hosnolarifin220@gmail.com>
2021-05-16 18:35:26 +0700

    Edit file seeder role

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class RoleSeeder extends Seeder
 {
@@ -13,6 +14,19 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Role::create([
+            'name' => 'admin',
+            'guard_name' => 'web'
+        ]);
+
+        Role::create([
+            'name' => 'dosen',
+            'guard_name' => 'web'
+        ]);
+
+        Role::create([
+            'name' => 'mahasiswa',
+            'guard_name' => 'web'
+        ]);
     }
 }
