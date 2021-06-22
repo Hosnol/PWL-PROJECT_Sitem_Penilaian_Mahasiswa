68a368934b629e569bf1967c8d4693f5f4be00f9
Hosnol Arifin <hosnolarifin220@gmail.com>
2021-06-22 10:42:59 +0700

    mengganti menu pada admin

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -7,19 +7,19 @@
             <a class="nav-link" href={{ route('dosen.index') }}>Dosen</a>
         </li>
         <li class="nav-item">
+            <a class="nav-link" href="{{ route('kelas.index') }}">Kelas</a>
+        </li>
+        <li class="nav-item">
             <a class="nav-link" href="{{ route('mahasiswa.index') }}">Mahasiswa</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('user.index') }}">User</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="{{ route('matakuliah.index') }}">Mata Kuliah</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('kelas.index') }}">Kelas</a>
-        </li>
-        <li class="nav-item">
             <a class="nav-link" href="{{ route('jadwal.index') }}">Jadwal</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('user.index') }}">User</a>
         </li>
     </ul>
 </div>
